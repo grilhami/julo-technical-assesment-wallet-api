2cc038621f85f5fe80d2df9e1bf462a0476674e3
grilhami <gilangrilhami@hotmail.co.uk>
2022-07-28 14:05:43 +0000

    Class for API Version 1
    The class that will be instantiate
    to register all the controllers via
    Express route.

---
+++ src/app/v1/index.ts
@@ -0,0 +1,37 @@
+import express, {Router} from 'express';
+
+interface IController {
+    path: string;
+    router: Router;
+}
+
+interface V1 {
+    _c: IController[];
+    path: string;
+    router: express.Router;
+}
+
+export default class APIV1 implements V1 {
+	path = "/v1";
+	router = express.Router();
+
+	// Arrays of controllers
+	_c = [];
+
+	constructor() {
+
+		// Initiate all controllers
+		this.initController(this._c);
+
+	}
+
+	/**
+	 * 
+	 * @param _c Controllers that implements IController interface
+	 */
+	private initController = (_c: IController[]) => {
+		_c.forEach((controller) => {
+			this.router.use(this.path, controller.router);
+		});
+	};
+}
